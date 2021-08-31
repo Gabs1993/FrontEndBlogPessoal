1351553c9b12d1e73dc9d96114c45fd02068ee65
unknown <gabrielcsantos1993@gmail.com>
2021-08-30 22:55:13 -0300

    front_blog

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
+import { TemaComponent } from './tema/tema.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo:'entrar',pathMatch: 'full'},
+
+  {path:'entrar', component:EntrarComponent}, 
+  {path: 'cadastrar', component:CadastrarComponent},
+  
+  {path: 'home', component:HomeComponent},
+  {path: 'tema', component:TemaComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
